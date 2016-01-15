724e76eb8c2c6afd8925bd2ad8127985f87e63be
mohakavi <amohak@iitk.ac.in>
2016-01-15 22:22:47 +0530

    1.1

--- calclex.py
+++ calclex.py
@@ -7,56 +7,47 @@
 import ply.lex as lex
 
 # List of token names.   This is always required
-tokens = (
-   'FOR','WHILE','IF','ELSE','DO','BREAK',
-   'TRUE','FALSE',
-   'TYPE','VAR','VAL',
-   'RETURN',   'IMPORT',   'CLASS',  'DEFINE', 'PRIVATE', 'PROTECTED','NULL',   'CASE',   'OBJECT','PACKAGE',
-   'PLUS',   'MINUS',   'TIMES',   'DIVIDE', 'MOD',  
-   '(',')','{','}', '[',']', ':'  , ',' ,'.'  ,';'  , '"'
+
+reserved = {
+	'if' : 'IF',
+	'else' : 'ELSE',
+	'for' : 'FOR',
+	'while' : 'WHILE',
+	'do' : 'DO',
+	'break' : 'BREAK',
+	'var' : 'VAR',
+	'val' : 'VAL',
+	'return' : 'RETURN',
+	'type' : 'TYPE',
+	'import' : 'IMPORT',
+	'class' : 'CLASS',
+	'private' : 'PRIVATE',
+	'protected' : 'PROTECTED',
+	'define' : 'DEFINE',
+	'null' : 'NULL',
+	'object' : 'OBJECT',
+	'package' : 'PACKAGE',
+	'case' : 'CASE'
+}
+
+tokens = [
+   'LPAREN', 'RPAREN',
+   'BINARY',
+   'AROP', 'RELOP','BINOP','LOGOP', 
    'IDENTIFIER',
-   'EQ','LEQ','GEQ','LESS','GREATER','NEQ',
-   'AND','OR','COMPLEMENT', 'XOR','LSHIFT','RSHIFT','RRSHIFT',
-   'LAND','LOR','LNOT',
    'COMMENT','COMMENT_BEGIN','COMMENT_END',
     'STRING','NUMBER'
-) # += Constanst int decimal real
+] + list(reserved.values())
+
 
 # Regular expression rules for simple tokens
-t_FOR     = r'for'
-t_WHILE   = r'while'
-t_IF      = r'if'
-t_ELSE    = r'else'
-t_DO      = r'do'
-t_BREAK   = r'break'
+t_BINARY = r'true|false'
 
-t_TRUE    = r'true'
-t_FALSE   = r'false'
+t_AROP   = r'[-%+*/]'
 
-t_TYPE    = r'type'
-t_VAR     = r'var'
-t_VAL     = r'val'
-t_RETURN  = r'return'
-t_IMPORT  = r'import'
-t_CLASS   = r'class'
-t_DEFINE  = r'define'
-t_PRIVATE = r'private'
-t_PROTECTED = r'protected'
-t_NULL      = r'null'
-t_CASE      = r'case'
-t_OBJECT    = r'object'
-t_PACKAGE   = r'package'
-
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_MOD     = r'\%'  ##
-
-
-t_'('     = r'\('
-t_')'     = r'\)'
-t_'{'     = r'\{'
+t_LPAREN     = r'\('
+t_RPAREN     = r'\)'
+'''t_'{'     = r'\{'
 t_'}'     = r'\}'
 t_'['     = r'\['
 t_']'     = r'\]'
@@ -65,42 +56,21 @@ t_','     = r'\,'
 t_';'     = r'\;'
 t_'.'     = r'\.'
 t_'"'     = r'\"'
+'''
+t_RELOP = r'==|<=|>=|<|>|!='
 
-t_EQ  = r'=='
-t_LEQ = r'<='
-t_GEQ = r'>='
-t_LESS = r'<' 
-t_GREATER = r'>'
-t_NEQ = r'!='
+t_BINOP = r'&|\||~|<<|>>|>>>'
 
-t_AND = r'&' 
-t_OR = r'|'
-t_COMPLEMENT = r'~'
-t_XOR = 'r^'
-t_LSHIFT = r'<<'
-t_RSHIFT = r'>>'
-t_RRSHIFT = 'r>>>'
+t_LOGOP = r'&&|!|\|\|'
 
-t_LAND = r'&'
-t_LOR = r'||'
-t_LNOT = r'!'
+t_STRING = r'".+"'
 
-t_COMMENT = r'//'
-t_COMMENT_BEGIN = r'/*'
-t_COMMENT_END = r'*/'
+t_NUMBER = r'[0-9]+.?[0-9]*E.[+-]?[0-9]*'
 
-t_STRING = r""
-t_NUMBER = r'[0-9]+.?[0-9]*E.[+-]?[0-9]*'   #2.
-
-t_IDENTIFIER = r'[_a-zA-Z][_a-zA-Z0-9]*'
-
-
-
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
+def t_IDENTIFIER(t):
+ 	r'[_a-zA-Z][_a-zA-Z0-9]*'
+ 	t.type = reserved.get(t.value,'IDENTIFIER')
+ 	return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -117,3 +87,20 @@ def t_error(t):
 
 # Build the lexer
 lexer = lex.lex()
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+  for
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok.value)
