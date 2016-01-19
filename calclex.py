5cc2f936d75f0fce7b9eb8579f544c02560df902
mohakavi <amohak@iitk.ac.in>
2016-01-19 15:21:46 +0530

    String issue resolved

--- calclex.py
+++ calclex.py
@@ -84,7 +84,7 @@ t_RELOP = r'==|<=|>=|<|>|!='
 t_BINOP = r'&|\||~|<<|>>|>>>'
 t_LOGOP = r'&&|!|\|\|'
 def t_STRING(t):
-    r'".+"'
+    r'"[^"\n]*"'
     t.value=t.value[1:-1]
     return t
 def t_CHAR(t):
