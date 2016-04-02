cab41b9b0e4a6c71749492732564e60a832d98db
Utkarsh <utkarsha@cse.iitk.ac.in>
2016-04-02 22:33:10 +0530

    Symbol Table

--- Assignment4/src/symboltable.py
+++ Assignment4/src/symboltable.py
@@ -1,9 +1,35 @@
-parentlist = []
-childlist = []
-emptyd = {}
-tablecount = 0
-tablelist = []
+#symbolTable is a dictionary of lists. type and value
+class Env:
+    tablecount = 1
+    def _init_(self, prev_env = None):
+        self.table = {}
+        self.code = []
+        self.id = tablecount
+        tablecount += 1
+        self.parent = prev_env
+        self.childlist = []
 
-def mktable(previous):
-    tablelist.append(emptyd)
-    
+        if(prev_env != None):
+            prev_env.childlist.append(self.id)
+
+    def add_entry(self, name, type, value=None):
+         if name in self.table:
+            print 'Error: Entry already present - ( ' + name + ' )'
+	    assert(False)
+        else:
+            self.table[name]= []
+            self.table[name].append(name)
+            self.table[name].append(type)
+
+    def present(self, name):
+        if name in self.table:
+            return True
+        else:
+            return False
+
+    def update_entry(self, name, key, attribute_value):
+        try:
+            self.table[name][key] = attribute_value
+            return True
+        except:
+            return False
