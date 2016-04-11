543cb5d5e29a84799e60e16c6461a4c1a124d2fc
techno <techno@HP-6730b>
2016-04-11 20:33:50 +0530

    return still remaining

---
+++ Assignment4/switch.scala
@@ -0,0 +1,12 @@
+object program{
+	var wflag, tflag, dflag, c :Int = 0;
+
+	c match {
+		case 1 => { }
+		case 2 => { wflag = 1;}
+		case 3 => { }
+		case 4 => { tflag = 1;}
+		case 5 => { }
+		case 6 => { dflag = 1;}
+	}
+}
