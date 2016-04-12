3d5dfa927278e20b078ada833620d3c79dbed0e3
arpang <arpan3495@gmail.com>
2016-04-13 01:18:15 +0530

    Arguments, local variables done. Testing not done.

---
+++ Assignment5/test/if.scala
@@ -0,0 +1,29 @@
+object program {
+	def gcd(a:Int, b:Int):Int = {
+		var g:Int = 0;
+
+		if (a < b){
+			a = a + b;
+			b = a - b;
+			a = a - b; 
+		}
+
+		if(b == 0){
+			g = a;
+		}
+		else{
+			g = a % b;
+
+			if(g != 0){
+				g = gcd(b,a%b);
+			}
+		}
+
+		return g;
+	}
+
+	def main():Unit = {
+		var g:Int = gcd(5,10);
+		var ar:Array[Int] = new Array[Int](50);
+	}
+}
